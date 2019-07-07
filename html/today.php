878a751ac8e061909cbc1406114ecce508dd3d74
sorakon <32617257+sorakon@users.noreply.github.com>
2019-07-06 20:02:31 -0400

    made changes add php

--- html/today.php
+++ html/today.php
@@ -1,18 +1,63 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"       
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">       
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">       
-  <head>       
-    <title>Today&rsquo;s Date</title>       
-    <meta http-equiv="content-type"       
-        content="text/html; charset=utf-8"/>       
-  </head>       
-  <body>       
-    <p>Today&rsquo;s date (according to this web server) is       
-      <?php       
-       
-      echo date('l, F dS Y.');       
-       
-      ?>       
-    </p>       
-  </body>       
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Resume - Xiaoyu Zhang</title>
+		<meta charset="utf-8" />
+        <link rel="shortcut icon" href="/images/favicon.png" type="image/png" >
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="../assets/css/aboutme.css" />
+		<noscript><link rel="stylesheet" href="../assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<section id="main">
+						<header>
+							<h1>Today's Date</h1>
+                                <p>Today's date (according to this web server) is   <br> 
+                                <?php echo date('l, F dS Y.');       ?>  <br>
+      
+                            
+
+
+						</header>
+
+								
+                        <br>
+                         <a title="Back to the front page" href="../index.html">To Front Page</a>
+					</section>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<ul class="copyright">
+							<li>&copy; Xiaoyu Zhang 2019</li>
+						</ul>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script>
+				if ('addEventListener' in window) {
+					window.addEventListener('load', function() { document.body.className = document.body.className.replace(/\bis-preload\b/, ''); });
+					document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
+				}
+			</script>
+        
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-143325646-1"></script>
+            <script>
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'UA-143325646-1');
+            </script>
+
+
+
+	</body>
 </html>
